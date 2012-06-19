cd7436682fa087846ac814769bd95e4178001cb7
Jay Phillips <jicksta+git@gmail.com>
2012-06-18 23:56:26 -0700

    Run 100 random solutions through the cost function and output the best ten

--- genetic.clj
+++ genetic.clj
@@ -1,13 +1,3 @@
-  
-;(defn print-schedule [flights solution]
-;  (let [[person [departs-index, returns-index]] (interleave people sample-solution)
-;         {depart-flight-num :flight-num} (nth flights departs-index)
-;         {return-flight-num :flight-num} (nth flights returns-index)]
-;    (prn (format
-;      "%10s should take Flight #%s and Flight #%s"
-;      (:name person) depart-flight-num return-flight-num))))
-
-
 (use '[clojure.string :only (split trim-newline)])
 (use 'clj-time.core)
 (use 'clj-time.format)
@@ -26,7 +16,7 @@
              (struct person "Les"     "OMA")
 ])
 
-(def csv-contents (slurp "schedule.txt"))
+(def csv-contents (slurp "schedule.csv"))
 
 (defn parse-date [date]
   "The CSV expresses flight times as HH:MM. This converts this String into a clj-time DateTime object."
@@ -43,7 +33,7 @@
   "Groups all flights by [from,to] vectors as the key with the list of matching flight structs as the values."
   (group-by (fn [flight] [(:from flight) (:to flight)]) flights))
 
-(def sample-solution [[1 4] [3 2] [7 3] [6 3] [2 4] [5 3]])
+;(def sample-solution [[1 4] [3 2] [7 3] [6 3] [2 4] [5 3]])
 
 (def flights (indexed-flights (parse-flights)))
 
@@ -78,9 +68,39 @@
              (:arrives-at departure)
              latest-arrival)))))))
 
+(defn random-departure-index-for-person [person]
+  (rand-int (count (flights [(:origin person) destination]))))
+
+(defn random-arrival-index-for-person [person]
+  (rand-int (count (flights [destination (:origin person)]))))
+
 (defn random-solution []
   (let [random-seq (repeatedly #(rand-int 100))]
-  (loop [solution])))
+  (for [person people]
+    [(random-departure-index-for-person person) (random-arrival-index-for-person person)])))
 
-;(println (first flights))
-(schedule-cost sample-solution)
+(def random-solutions
+  (sort-by schedule-cost (take 100 (repeatedly random-solution))))
+
+(println "Solutions:")
+(dorun (for [solution (take 10 random-solutions)]
+  (println (schedule-cost solution) (flatten solution))))
+
+
+
+
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+  
+
+;(defn print-schedule [flights solution]
+;  (let [[person [departs-index, returns-index]] (interleave people sample-solution)
+;         {depart-flight-num :flight-num} (nth flights departs-index)
+;         {return-flight-num :flight-num} (nth flights returns-index)]
+;    (prn (format
+;      "%10s should take Flight #%s and Flight #%s"
+;      (:name person) depart-flight-num return-flight-num))))
+
